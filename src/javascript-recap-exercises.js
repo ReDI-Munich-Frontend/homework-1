467acc2349ddd3c8c44c66cb642bdfd4e3160171
Savio van Hoi <savio.vanhoi@gmail.com>
2021-03-13 13:08:32 +0100

    feat: add basic tests for array methods

---
+++ src/javascript-recap-exercises.js
@@ -0,0 +1,152 @@
+// Array methods
+
+/** 1. Exercise: Counting with Array.forEach()
+ *
+ * Given an array with numbers
+ * write a function
+ * which counts all values in the array
+ * and returns the sum
+ * The solution must use `forEach`.
+ *
+ * i.e:
+ * const numbers =[1,2,3]
+ * sumForEach(numbers) should return 6
+ */
+
+function sumForEach(array) {
+  let count = 0;
+
+  // your code here
+
+  return count;
+}
+
+/** 2. Exercise: Returning a new array with Array.forEach()
+ *
+ * Given an array which contains objects
+ * with firstName and lastName
+ * write a function
+ * which returns a new array with full name
+ * The solution must use `forEach`.
+ *
+ * i.e:
+ * const employees = [
+ *  {firstName: 'Alice', lastName: 'Airy' },
+ *  {firstName: 'Bob', lastName: 'Bubble'}
+ * ]
+ * fullNameForEach(employees) should return
+ * ['Alice Airy', 'Bob Bubble']
+ */
+
+function fullNameForEach(array) {
+  let fullNameArray = [];
+
+  // your code here
+
+  return fullNameArray;
+}
+
+/** 3. Exercise: Returning a new array with Array.map()
+ *
+ * Given an array which contains objects
+ * with firstName and lastName
+ * write a function
+ * which returns a new array with full name
+* The solution must use `map`.
+
+ * i.e:
+ * const employees = [
+ *  {firstName: 'Alice', lastName: 'Airy' },
+ *  {firstName: 'Bob', lastName: 'Bubble'}
+ * ]
+ * fullNameMap(employees) should return
+ * ['Alice Airy', 'Bob Bubble']
+ */
+
+function fullNameMap(array) {
+  // your code here  ;
+}
+
+/** 4. Exercise: Filter an array with numbers using Array.filter()
+ *
+ * Given an array which contains numbers
+ * write a function
+ * which returns a new array with only even numbers
+* The solution must use `filter`.
+
+ * i.e:
+ * const numbers = [1,2,3,4,5,6]
+ * filterByEven(employees) should return [2,4,6]
+ */
+function filterByEven(array) {
+  // your code here  ;
+}
+
+/** 5. Exercise: Filter an array with objects using Array.filter()
+ *
+ * Given an array which contains objects
+ * with name and status
+ * write a function
+ * which returns a new array which only objects where status equals 'accepted' 
+* The solution must use `filter`.
+
+ * i.e:
+ * const invitations = [
+ *  {name: 'Alice', status: 'invited'},
+ *  {name: 'Bob', status: 'invited'},
+ *  {name: 'Carol', status: 'accepted'},
+ *  {name: 'Dan', status: 'rejected'},
+ * ]
+ * filterByAccepted(employees) should return [{name: 'Carol', status: 'accepted'}]
+ */
+function filterByAccepted(array) {
+  // your code here  ;
+}
+
+/** 6. Exercise: Counting using Array.useReduce()
+ *
+ * Given an array with numbers
+ * write a function
+ * which counts all values in the array
+ * and returns the sum
+ * The solution must use `forEach`.
+ *
+ * i.e:
+ * const numbers =[1,2,3]
+ * sumReduce(numbers) should return 6
+ */
+function sumReduce(array) {
+  // your code here  ;
+}
+
+/** 7. Exercise: Transforming using Array.useReduce()
+ *
+ * Given an array with objects
+ * with name and ids
+ * write a function
+ * which returns a new object where id is the key
+ *
+ * i.e:
+ * const employees = [
+ *  {name: 'Alice', id: 3},
+ *  {name: 'Bob', id: 7}
+ *]
+ * reduceArrayToObject(numbers) should return
+ *
+ * {
+ *     3: { name: "Alice", id: 3 },
+ *     7: { name: "Bob", id: 7 },
+ *   }
+ */
+function reduceArrayToObject(array) {
+  // your code here  ;
+}
+
+export {
+  sumForEach,
+  fullNameForEach,
+  fullNameMap,
+  filterByEven,
+  filterByAccepted,
+  reduceArrayToObject,
+};
---
+++ src/javascript-recap-exercises.test.js
@@ -0,0 +1,95 @@
+import {
+  sumForEach,
+  fullNameForEach,
+  fullNameMap,
+  filterByEven,
+  filterByAccepted,
+  reduceArrayToObject,
+} from "./javascript-recap-exercises";
+
+describe("1. Exercise: Counting with Array.forEach()", () => {
+  it("given [1,2,3] it should return 6", () => {
+    const fixture = [1, 2, 3];
+
+    expect(sumForEach(fixture)).toEqual(6);
+  });
+
+  it("given [5, 6, 3, 7, -1, 10, 12] it should return 42", () => {
+    const fixture = [5, 6, 3, 7, -1, 10, 12];
+
+    expect(sumForEach(fixture)).toEqual(42);
+  });
+});
+
+describe("2. Exercise: Returning a new array with Array.forEach()", () => {
+  it("given an array of employees with firstName and lastName it should return a new array with full name", () => {
+    const fixture = [
+      { firstName: "Luke", lastName: "Skywalker" },
+      { firstName: "Ahsoka", lastName: "Tano" },
+    ];
+
+    expect(fullNameForEach(fixture)).toEqual(["Luke Skywalker", "Ahsoka Tano"]);
+  });
+});
+
+describe("3. Exercise: Returning a new array with Array.map()", () => {
+  it("given an array of employees with firstName and lastName it should return a new array with full name", () => {
+    const fixture = [
+      { firstName: "Luke", lastName: "Skywalker" },
+      { firstName: "Ahsoka", lastName: "Tano" },
+    ];
+
+    expect(fullNameMap(fixture)).toEqual(["Luke Skywalker", "Ahsoka Tano"]);
+  });
+});
+
+describe("4. Exercise: Filter an array with numbers using Array.filter()", () => {
+  it("given an array with numbers it should return only even numbers", () => {
+    const fixture = [1, 2, 3, 4, 5, 6];
+
+    expect(filterByEven(fixture)).toEqual([2, 4, 6]);
+  });
+});
+
+describe("5. Exercise: Filter an array with objects using Array.filter()", () => {
+  it("given an array with objects containing status it should return only 'accepted' ones", () => {
+    const fixture = [
+      { name: "Alice", status: "invited" },
+      { name: "Bob", status: "invited" },
+      { name: "Carol", status: "accepted" },
+      { name: "Dan", status: "rejected" },
+    ];
+
+    expect(filterByAccepted(fixture)).toEqual([
+      { name: "Carol", status: "accepted" },
+    ]);
+  });
+});
+
+describe("6. Exercise: Counting using Array.useReduce()", () => {
+  it("given [1,2,3] it should return 6", () => {
+    const fixture = [1, 2, 3];
+
+    expect(sumForEach(fixture)).toEqual(6);
+  });
+
+  it("given [5, 6, 3, 7, -1, 10, 12] it should return 42", () => {
+    const fixture = [5, 6, 3, 7, -1, 10, 12];
+
+    expect(sumForEach(fixture)).toEqual(42);
+  });
+});
+
+describe("7. Exercise: Transforming using Array.useReduce()", () => {
+  it("given an array with object ids it should return a new object with id keys", () => {
+    const fixture = [
+      { name: "Alice", id: 3 },
+      { name: "Bob", id: 7 },
+    ];
+
+    expect(reduceArrayToObject(fixture)).toEqual({
+      3: { name: "Alice", id: 3 },
+      7: { name: "Bob", id: 7 },
+    });
+  });
+});
